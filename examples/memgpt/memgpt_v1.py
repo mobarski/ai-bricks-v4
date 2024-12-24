989d45345350cbe7519b0068f70ff627b119f998
Maciej Obarski <mobarski@gmail.com>
2024-12-25 00:41:54 +0100

    WIP memgpt v1

--- examples/memgpt/memgpt_v1.py
+++ examples/memgpt/memgpt_v1.py
@@ -5,16 +5,23 @@ client = aibricks.client(model='xai:grok-beta')
 config = aibricks.load_config(Path(__file__).parent / 'memgpt_v1.yaml')
 
 messages = [{'role': 'system', 'content': config.render('system_prompt')}]
+memory = ''
 
 
 def chat_turn(user_input):
-    global messages
+    global messages, memory
     messages += [{'role': 'user', 'content': user_input}]
     response = client.chat(messages=messages)
     content = response['choices'][0]['message']['content']
-    print(content)
+    print(content) # XXX
     # TODO: parse xml
-    # TODO: call tools
+    for tool, kw in []:
+        if tool == 'respond':
+            print('AI:', kw['message'])
+        elif tool == 'memory.add':
+            memory += kw['new'] + '\n'
+        elif tool == 'memory.replace':
+            memory = memory.replace(kw['old'], kw['new'])
     # TODO: update prompt
 
 
@@ -22,3 +29,4 @@ if __name__ == '__main__':
     chat_turn("Hi! My name is Maciej and I'm from Poland!")
     chat_turn("I was born in 1981 and learned to program when I was 8.")
     chat_turn("Can you guess my first computer and programming language?")
+    chat_turn("Well, it was Timex 2048 and BASIC.")
