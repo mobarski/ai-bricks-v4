6b6e1fe3fef12732cd5e4a5e76602aeab862795a
Maciej Obarski <mobarski@gmail.com>
2024-12-25 00:22:37 +0100

    WIP memgpt v1

--- examples/memgpt/memgpt_v1.py
+++ examples/memgpt/memgpt_v1.py
@@ -4,12 +4,12 @@ from pathlib import Path
 client = aibricks.client(model='xai:grok-beta')
 config = aibricks.load_config(Path(__file__).parent / 'memgpt_v1.yaml')
 
-messages = [
-    {'role': 'system', 'content': config.render('system_prompt')},
-]
+messages = [{'role': 'system', 'content': config.render('system_prompt')}]
 
 
-def main_loop():
+def chat_turn(user_input):
+    global messages
+    messages += [{'role': 'user', 'content': user_input}]
     response = client.chat(messages=messages)
     content = response['choices'][0]['message']['content']
     print(content)
@@ -19,5 +19,6 @@ def main_loop():
 
 
 if __name__ == '__main__':
-    messages += [{'role': 'user', 'content': "Hi! My name is Maciej and I'm from Poland!!"}]
-    main_loop()
+    chat_turn("Hi! My name is Maciej and I'm from Poland!")
+    chat_turn("I was born in 1981 and learned to program when I was 8.")
+    chat_turn("Can you guess my first computer and programming language?")
