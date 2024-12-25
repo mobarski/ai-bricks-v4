dc3387ea36bce3b423afb654e95417695e241652
Maciej Obarski <mobarski@gmail.com>
2024-12-25 12:01:00 +0100

    fix naming in utils.xml

--- aibricks/utils/xml.py
+++ aibricks/utils/xml.py
@@ -2,20 +2,21 @@ import xml.etree.ElementTree as ET
 import re
 
 
-def top_level_tags(xml_string):
+def extract_xml_chunks(text):
     """
-    Extract top-level tags from XML string.
+    Extract top-level xml tags from a text.
     """
-    matches = re.findall(r'(<(\w+)\W.*?</\2>)', xml_string, re.DOTALL)
+    matches = re.findall(r'(<(\w+)\W.*?</\2>)', text, re.DOTALL)
     return [match[0] for match in matches]
 
 
-def parse_xml(xml_string):
+def parse_xml(text):
     """
-    Parse XML string into a list of tuples (tag, attributes, keyword arguments).
+    Parse top level xml tags within a text into a list of tuples
+    (tag, attributes, keyword arguments).
     """
     out = []
-    for xml in top_level_tags(xml_string):
+    for xml in extract_xml_chunks(text):
         et = ET.fromstring(xml)
         kw = {}
         for child in et:
