926b57fc9fc9ab76e54e85ba31b42d9611a0b260
Maciej Obarski <mobarski@gmail.com>
2025-01-11 22:25:20 +0100

    WIP logprobs test

--- tests/test_api_openrouter.py
+++ tests/test_api_openrouter.py
@@ -11,6 +11,16 @@ def test_chat_stream():
     common.test_chat_stream(TEST_MODEL)
 
 
+def test_chat_logprobs():
+    common.test_chat(TEST_MODEL, logprobs=True, top_logprobs=None)  # ERROR: ignored
+
+
+def test_chat_top_logprobs():
+    common.test_chat(TEST_MODEL, logprobs=True, top_logprobs=3)  # ERROR: ignored
+
+
 if __name__ == "__main__":
     test_chat()
     test_chat_stream()
+    test_chat_logprobs()
+    test_chat_top_logprobs()
